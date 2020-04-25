2bd3592ff7b3c5154dac200147dc8411d9889835
Sheriff Shittu <yabacoder@gmail.com>
2020-04-25 19:32:56 +0100

    response fixed with promises

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,11 @@
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable no-undef */
-import history from '../history';
+// import history from '../history';
 import api from '../api/allapi';
 
 export const fetchProducts = () => async (dispatch) => {
-  const response = await api.getData('products/v2/list?country=US&currency=USD&sort=freshness&lang=en-US&sizeSchema=US&offset=0&categoryId=4209&limit=48&store=US'); // //It's working even without the parameter, so it's not doing anything
-  // const response = await api.getData('products/v2/list'); 
-
-  dispatch({ type: 'FETCH_PRODUCTS', payload: response.products });
+  api.getData('products/v2/list?country=US&currency=USD&sort=freshness&lang=en-US&sizeSchema=US&offset=0&categoryId=4209&limit=48&store=US')
+    .then((response) => {
+      dispatch({ type: 'FETCH_PRODUCTS', payload: response.data.products });
+    })
 };
--- src/reducers/productReducer.js
+++ src/reducers/productReducer.js
@@ -2,7 +2,6 @@ export default (state = [], action) => {
   switch (action.type) {
     case 'FETCH_PRODUCTS':
       return [...state, action.payload];
-
     default:
       return state;
   }
